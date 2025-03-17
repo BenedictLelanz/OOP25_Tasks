ca5f382ff173738252b754e9709be945c78722eb
Benedict Lelanz <lelanz21042003@gmail.com>
2025-03-17 09:32:37 +0100

    modify lecture content inheritance

--- src/main/abstraction/inheritance/lecture/Main.java
+++ src/main/abstraction/inheritance/lecture/Main.java
@@ -4,20 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
+    
         Children children = new Children();
-        children.childrenMethod1();
+
+        //children.parentMethod2();
+
+        //children.i;
+
+        //System.out.println(children);
+
+        Children children2 = children;
+
+        System.out.println(children == children2);
+        System.out.println(children.equals(children2));
+
+        new Children(3);
+        //children.childrenMethod1();
         children.parentMethod1();
-        children.parentMethod2();
+        //children.parentMethod2();
         
         // Geht das?
-        //Children children = new Parent();
+       // Children children = new Parent();
         
         // Geht das?
-        //doSthWithParent(children);
+        doSthWithParent(children);
 
         Parent p = children;
         // Geht das?
-        //Children cc = p;
+        Children cc = (Children) p;
+
+        //Parent parent = new Parent();
+        
+        //Children c = (Children) parent;
 
         // ... expliziter Cast
 
