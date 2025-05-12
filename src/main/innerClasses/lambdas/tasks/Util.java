f9f8f54b79f1182e43c8e1c20b707ab1699b3374
Benedict Lelanz <lelanz21042003@gmail.com>
2025-05-12 10:39:05 +0200

    update tasks

---
+++ src/main/innerClasses/lambdas/tasks/Util.java
@@ -0,0 +1,21 @@
+package main.innerClasses.lambdas.tasks;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Util {
+
+    static <T> T reduce(List<T> elements, Link<T> link) {
+        
+    }
+
+    static <T, R> List<R> map(List<T> elements, Function<T, R> mapper) {
+
+    }
+
+    static <T> List<T> filter(List<T> elements, Predicate<T> pr) {
+        
+    }
+    
+}
